08da442f745263bd5ef58ede892a8e81a801c9df
Md Monowar <157671493+monowar-1@users.noreply.github.com>
2024-03-15 22:02:27 +0600

    Update min.js

--- min.js
+++ min.js
@@ -1,1 +1,9 @@
+const A = [10, 1001, 'AB', 150, 'AC', 'AD', 30, 120, 'AF'];
 
+const Strings = A.filter(item => typeof item === 'string');
+
+const Numbers = A.filter(item => typeof item === 'number');
+
+console.log("New Array1:", Strings);
+
+console.log("New Array2:", Numbers);
